654dd120437a894871cbf960e25d020d777f482b
Matias <matiascantoni@hotmail.com>
2026-05-16 19:00:15 -0300

    tests presentacion y dominio

---
+++ src/test/java/com/tallerwebi/dominio/servicio/ServicioUsuarioTest.java
@@ -0,0 +1,66 @@
+package com.tallerwebi.dominio.servicio;
+
+import com.tallerwebi.dominio.Usuario;
+import com.tallerwebi.dominio.RepositorioUsuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
+public class ServicioUsuarioTest {
+
+    private RepositorioUsuario repositorioMock;
+    private ServicioUsuario servicio;
+
+    @BeforeEach
+    public void init() {
+        repositorioMock = mock(RepositorioUsuario.class);
+        servicio = new ServicioUsuarioImpl(repositorioMock);
+    }
+
+    @Test
+    public void queSePuedaObtenerUnPerfilPorId() {
+        Long idBuscado = 1L;
+        Usuario usuarioSimulado = new Usuario();
+        when(repositorioMock.buscarPorId(idBuscado)).thenReturn(usuarioSimulado);
+
+        Usuario resultado = servicio.obtenerPerfil(idBuscado);
+
+        assertNotNull(resultado, "El usuario no debe ser nulo");
+        verify(repositorioMock, times(1)).buscarPorId(idBuscado);
+    }
+
+    @Test
+    public void queAlEliminarUnUsuarioSuEstadoPaseAInactivo() {
+        Usuario usuarioSimulado = new Usuario();
+        usuarioSimulado.setActivo(true);
+
+        when(repositorioMock.buscarPorId(1L)).thenReturn(usuarioSimulado);
+
+        servicio.eliminar(1L);
+
+        assertFalse(usuarioSimulado.getActivo(), "El usuario debe estar inactivo");
+    }
+
+    @Test
+    public void queDevuelvaNullSiSeBuscaUnPerfilQueNoExiste() {
+        Long idInexistente = 1L;
+        when(repositorioMock.buscarPorId(idInexistente)).thenReturn(null);
+
+        Usuario resultado = repositorioMock.buscarPorId(idInexistente);
+
+        assertNull(resultado, "El resultado debe ser null si el usuario no existe");
+    }
+
+    @Test
+    public void queNoOcurraNingunErrorAlIntentarEliminarUnUsuarioInexistente() {
+        Long idInexistente = 99L;
+        when(repositorioMock.buscarPorId(idInexistente)).thenReturn(null);
+        servicio.eliminar(idInexistente);
+
+        verify(repositorioMock, times(1)).buscarPorId(idInexistente);
+    }
+}
