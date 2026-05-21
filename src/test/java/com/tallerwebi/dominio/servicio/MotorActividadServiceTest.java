a6a6af9659d4a3b3933f8a01c82f2260cfed31d6
Mercado006 <smercado999@alumno.unlam.edu.ar>
2026-05-21 16:53:42 -0300

    Se agregó un test para comprobar que la mascota pueda tomar el estado durmiendo

--- src/test/java/com/tallerwebi/dominio/servicio/MotorActividadServiceTest.java
+++ src/test/java/com/tallerwebi/dominio/servicio/MotorActividadServiceTest.java
@@ -76,6 +76,37 @@ public class MotorActividadServiceTest {
     assertThat(estado3, equalTo(EstadoMascota.CAMINANDO));
     assertThat(estado4, equalTo(EstadoMascota.CORRIENDO));
   }
+  
+  @Test
+  public void debePoderLlegarADurmiendoDesdeCorriendoDeAPoco() {
+      LecturaSensor corriendo = new LecturaSensor();
+      corriendo.setFrecuenciaCardiaca(130);
+      corriendo.setAccelX(10.0);
+      corriendo.setAccelY(10.0);
+      corriendo.setAccelZ(10.0);
+      corriendo.setGyroX(6.0);
+      corriendo.setGyroY(6.0);
+      corriendo.setGyroZ(6.0);
+
+      LecturaSensor dormido = new LecturaSensor();
+      dormido.setFrecuenciaCardiaca(80);
+      dormido.setAccelX(0.1);
+      dormido.setAccelY(0.1);
+      dormido.setAccelZ(0.1);
+      dormido.setGyroX(0.1);
+      dormido.setGyroY(0.1);
+      dormido.setGyroZ(0.1);
+
+      EstadoMascota estado1 = motorActividadService.analizar(mascota, corriendo);
+      EstadoMascota estado2 = motorActividadService.analizar(mascota, dormido);
+      EstadoMascota estado3 = motorActividadService.analizar(mascota, dormido);
+      EstadoMascota estado4 = motorActividadService.analizar(mascota, dormido);
+
+      assertThat(estado1, equalTo(EstadoMascota.CORRIENDO));
+      assertThat(estado2, equalTo(EstadoMascota.CAMINANDO));
+      assertThat(estado3, equalTo(EstadoMascota.REPOSO));
+      assertThat(estado4, equalTo(EstadoMascota.DURMIENDO));
+  }
 
   @Test
   public void siLaLecturaSigueIgualDebeAvanzarAlSiguienteEstadoParaNoRepetir() {
